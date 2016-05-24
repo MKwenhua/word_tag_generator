930a72c921abdbbf91f5248a7448144f5fc6725f
Peter halloran <mustardbeard28@gmail.com>
2016-05-24 14:47:28 -0500

    . .

--- dist/word_tags.js
+++ dist/word_tags.js
@@ -2,57 +2,56 @@
 	'use strict';
 
 	window.wordScan = window.wordScan || {};
-  function wordTagHtml(nm){
-  	return ['<div data-keyy="', nm,'" class="tag_previewZZ66">',nm,'</div>'].join('');
-  };
+
+	function wordTagHtml(nm){
+		return ['<div data-keyy="', nm,'" class="tag_previewZZ66">',nm,'</div>'].join('');
+	};
+
 	window.wordScan.taggs = {
-		current: null, 
+		current: null,
 		tagNameLists: [],
 		htmlScans: 0,
 		tagContainer: null,
 		domReads: 0,
 		fileReads: 0,
 		wordLists: {},
-		htmlStrings: {},  
+		htmlStrings: {},
 		createName: function(name){
 			var _nm = '';
-
 			switch(name) {
-				case 'HTML SCAN': 
+				case 'HTML SCAN':
 					this.htmlScans += 1;
-				 _nm = 'HTML_SCAN_'+ String(this.htmlScans);
+					_nm = 'HTML_SCAN_'+ String(this.htmlScans);
 					break;
 				case 'DOM_Content':
 					this.domReads += 1;
-				 _nm = 'HTML_SCAN_'+ String(this.domReads);
+					_nm = 'HTML_SCAN_'+ String(this.domReads);
 					break;
 				default:
 					this.fileReads += 1;
 					_nm = name;
 			}
-
 			return _nm;
-			
 		},
 		addToDom: function(tagg){
 			this.current = tagg;
 			this.htmlStrings[tagg.namee] = wordTagHtml(tagg.namee);
 			var htmls = this.htmlStrings;
       this.tagContainer.innerHTML = this.tagNameLists.reduce(function(strA, keyy){
-				strA.push(htmls[keyy]); 
-			},[]).join('');		
+				return strA.push(htmls[keyy]);
+			},[]).join('');
 
 		},
 		addTag: function(tagg, name) {
 			var newName = this.createName(name);
-      tagg.namee = newName;
-      var nameList = this.tagNameLists;
-      this.tagNameLists = nameList.filter(function(item){
-      	return item !== newName;
-      });
-      this.tagNameLists.push(newName);
-      this.wordLists[newName] = tagg;
-      this.addToDom(tagg);
+			tagg.namee = newName;
+			var nameList = this.tagNameLists;
+			this.tagNameLists = nameList.filter(function(item){
+				return item !== newName;
+			});
+			this.tagNameLists.push(newName);
+			this.wordLists[newName] = tagg;
+			this.addToDom(tagg);
 		}
 	};
 
