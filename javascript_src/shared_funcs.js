f3934d0c2c0bca0a649de05dfec4ad104e6ff3af
Peter halloran <mustardbeard28@gmail.com>
2016-05-25 11:58:18 -0500

    formatting

--- javascript_src/shared_funcs.js
+++ javascript_src/shared_funcs.js
@@ -1,41 +1,41 @@
 (function (window) {
-  'use strict';
-  window.wordScan = window.wordScan || {};
+   'use strict';
+   window.wordScan = window.wordScan || {};
 
-  function FsTools() {
-    this.throttle = function (callback, limit) {
-      var wait = false;
-      return function () {
-        if (!wait) {
-          callback.call();
-          wait = true;
-          setTimeout(function () {
-            wait = false;
-          }, limit);
-        }
+   function FsTools() {
+      this.throttle = function (callback, limit) {
+         var wait = false;
+         return function () {
+            if (!wait) {
+               callback.call();
+               wait = true;
+               setTimeout(function () {
+                  wait = false;
+               }, limit);
+            }
+         }
       }
-    }
-    this.debounce = function (func, wait, immediate) {
-      var timeout;
-      return function () {
-        var context = this,
-          args = arguments;
-        clearTimeout(timeout);
-        timeout = setTimeout(function () {
-          timeout = null;
-          if (!immediate) func.apply(context, args);
-        }, wait);
-        if (immediate && !timeout) func.apply(context, args);
+      this.debounce = function (func, wait, immediate) {
+         var timeout;
+         return function () {
+            var context = this,
+               args = arguments;
+            clearTimeout(timeout);
+            timeout = setTimeout(function () {
+               timeout = null;
+               if (!immediate) func.apply(context, args);
+            }, wait);
+            if (immediate && !timeout) func.apply(context, args);
+         };
       };
-    };
-  };
-  FsTools.prototype.addDebounce = function (elem, func, time, type) {
-    elem.addEventListener(type, this.debounce(func, time));
-  };
-  FsTools.prototype.addThrottle = function (elem, func, time, type) {
-    elem.addEventListener(type, this.debounce(func, time));
-  };
+   };
+   FsTools.prototype.addDebounce = function (elem, func, time, type) {
+      elem.addEventListener(type, this.debounce(func, time));
+   };
+   FsTools.prototype.addThrottle = function (elem, func, time, type) {
+      elem.addEventListener(type, this.debounce(func, time));
+   };
 
-  window.wordScan.tools = new FsTools();
+   window.wordScan.tools = new FsTools();
 
 })(window);
--- javascript_src/tag_model.js
+++ javascript_src/tag_model.js
@@ -1,61 +1,61 @@
-(function(window) {
-  'use strict';
+(function (window) {
+   'use strict';
 
-  window.wordScan = window.wordScan || {};
+   window.wordScan = window.wordScan || {};
 
-  function wordTagHtml(nm) {
-    return ['<div data-keyy="', nm, '" class="tag_previewZZ66">',
-      nm.split('.')[0].replace(/[_-]/g, ' '),
-      '</div>'
-    ].join('');
-  };
+   function wordTagHtml(nm) {
+      return ['<div data-keyy="', nm, '" class="tag_previewZZ66">',
+         nm.split('.')[0].replace(/[_-]/g, ' '),
+         '</div>'
+      ].join('');
+   };
 
-  window.wordScan.taggs = {
-    current: null,
-    tagNameLists: [],
-    htmlScans: 0,
-    tagContainer: null,
-    domReads: 0,
-    fileReads: 0,
-    wordLists: {},
-    htmlStrings: {},
-    createName: function(name) {
-      var _nm = '';
-      switch (name) {
-        case 'HTML SCAN':
-          this.htmlScans += 1;
-          _nm = 'HTML_SCAN_' + String(this.htmlScans);
-          break;
-        case 'DOM_Content':
-          this.domReads += 1;
-          _nm = 'DOM_' + String(this.domReads);
-          break;
-        default:
-          this.fileReads += 1;
-          _nm = name;
+   window.wordScan.taggs = {
+      current: null,
+      tagNameLists: [],
+      htmlScans: 0,
+      tagContainer: null,
+      domReads: 0,
+      fileReads: 0,
+      wordLists: {},
+      htmlStrings: {},
+      createName: function (name) {
+         var _nm = '';
+         switch (name) {
+         case 'HTML SCAN':
+            this.htmlScans += 1;
+            _nm = 'HTML_SCAN_' + String(this.htmlScans);
+            break;
+         case 'DOM_Content':
+            this.domReads += 1;
+            _nm = 'DOM_' + String(this.domReads);
+            break;
+         default:
+            this.fileReads += 1;
+            _nm = name;
+         }
+         return _nm;
+      },
+      addToDom: function (tagg) {
+         this.current = tagg;
+         this.htmlStrings[tagg.namee] = wordTagHtml(tagg.namee);
+         var htmls = this.htmlStrings;
+         this.tagContainer.innerHTML = this.tagNameLists.reduce(function (str, keyy) {
+            return str + htmls[keyy];
+         }, '');
+
+      },
+      addTag: function (tagg, name) {
+         var newName = this.createName(name);
+         tagg.namee = newName;
+         var nameList = this.tagNameLists;
+         this.tagNameLists = nameList.filter(function (item) {
+            return item !== newName;
+         });
+         this.tagNameLists.push(newName);
+         this.wordLists[newName] = tagg;
+         this.addToDom(tagg);
       }
-      return _nm;
-    },
-    addToDom: function(tagg) {
-      this.current = tagg;
-      this.htmlStrings[tagg.namee] = wordTagHtml(tagg.namee);
-      var htmls = this.htmlStrings;
-      this.tagContainer.innerHTML = this.tagNameLists.reduce(function(str, keyy) {
-        return str + htmls[keyy];
-      }, '');
-
-    },
-    addTag: function(tagg, name) {
-      var newName = this.createName(name);
-      tagg.namee = newName;
-      var nameList = this.tagNameLists;
-      this.tagNameLists = nameList.filter(function(item) {
-        return item !== newName;
-      });
-      this.tagNameLists.push(newName);
-      this.wordLists[newName] = tagg;
-      this.addToDom(tagg);
-    }
-  };
+   };
 
 })(window);
